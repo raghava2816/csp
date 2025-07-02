4e441c60642deb4fc1b9b5775247a597ea55cb72
raghava2816 <168261246+raghava2816@users.noreply.github.com>
2025-07-02 23:29:15 +0530

    Update server.js

--- sos-backend/server.js
+++ sos-backend/server.js
@@ -9,6 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
